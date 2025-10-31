62f57d8ff061f7857a46ad1fc8b83b00a9cfeb24
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-31 14:49:09 +0700

    Membuat `FormDataDiri` dan variabelnya

--- app/src/main/java/com/example/userinput/TextEdit.kt
+++ app/src/main/java/com/example/userinput/TextEdit.kt
@@ -1,2 +1,20 @@
 package com.example.userinput
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun FormDataDiri(modifier: Modifier = Modifier
+) {
+    //variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+
+}
