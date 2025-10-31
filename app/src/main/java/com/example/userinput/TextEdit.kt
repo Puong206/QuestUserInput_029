e03035a1d5bdd9cc1ba97230f018f3aae6a840d3
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-31 14:53:18 +0700

    Membuat `Column` dan mengatur layout

--- app/src/main/java/com/example/userinput/TextEdit.kt
+++ app/src/main/java/com/example/userinput/TextEdit.kt
@@ -1,11 +1,17 @@
 package com.example.userinput
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 
 
 @Composable
@@ -22,4 +28,10 @@ fun FormDataDiri(modifier: Modifier = Modifier
     var jenis by remember { mutableStateOf("") }
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+    }
 }
