97a02ddaf618edd97199eaa4856c6e87772285da
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-01 07:56:57 +0700

    Membuat variabel untuk form registrasi

--- app/src/main/java/com/example/userinput/UserInput.kt
+++ app/src/main/java/com/example/userinput/UserInput.kt
@@ -1,10 +1,14 @@
 package com.example.userinput
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 val PlusJakartaSans = FontFamily(
     Font(R.font.plusjakartasans_extralight, FontWeight.ExtraLight),
@@ -19,5 +23,21 @@ val PlusJakartaSans = FontFamily(
 @Composable
 fun Register(modifier: Modifier = Modifier)
 {
+    var textNama by remember { mutableStateOf("") }
+    var textAsal by remember { mutableStateOf("") }
+    var textTgl by remember { mutableStateOf("") }
+    var textRt by remember { mutableStateOf("") }
+    var textRw by remember { mutableStateOf("") }
+    var textUsia by remember { mutableStateOf("") }
+    var textGender by remember { mutableStateOf("") }
+
+    var nama by remember { mutableStateOf("") }
+    var asal by remember { mutableStateOf("") }
+    var tgl by remember { mutableStateOf("") }
+    var rt by remember { mutableStateOf("") }
+    var rw by remember { mutableStateOf("") }
+    var usia by remember { mutableStateOf("") }
+    val gender by remember { mutableStateOf("") }
+
 
 }
