17829f2c7842b3ca47c2453460a68e53902a5ef4
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-01 14:16:16 +0700

    Membuat variabel `PlusJakartaSans` untuk FontFamily

--- app/src/main/java/com/example/userinput/UserInput.kt
+++ app/src/main/java/com/example/userinput/UserInput.kt
@@ -1,2 +1,18 @@
 package com.example.userinput
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val PlusJakartaSans = FontFamily(
+    Font(R.font.plusjakartasans_extralight, FontWeight.ExtraLight),
+    Font(R.font.plusjakartasans_light, FontWeight.Light),
+    Font(R.font.plusjakartasans_regular, FontWeight.Normal),
+    Font(R.font.plusjakartasans_medium, FontWeight.Medium),
+    Font(R.font.plusjakartasans_semibold, FontWeight.SemiBold),
+    Font(R.font.plusjakartasans_bold, FontWeight.Bold),
+    Font(R.font.plusjakartasans_extrabold, FontWeight.ExtraBold)
+)
+
+@Composable
